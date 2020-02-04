ded0743e462903074c5503d26f7c45df3e991da7
Steven VanWoerkom <vanwoerkom.steve@gmail.com>
2020-02-03 23:02:51 -0500

    add started files

---
+++ many-to-many/seed.sql
@@ -0,0 +1,5 @@
+DROP DATABASE IF EXISTS library;
+CREATE DATABASE library;
+
+\c library;
+\i schema.sql; 
---
+++ one-to-many/seed.sql
@@ -0,0 +1,5 @@
+DROP DATABASE IF EXISTS cities_db;
+CREATE DATABASE cities_db;
+
+\c cities_db;
+\i schema.sql;
